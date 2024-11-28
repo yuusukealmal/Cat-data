eb576fc91ac9cae94bd501230370e2d03e7f59fe
侑介 <spark9487111@gmail.com>
2024-11-28 22:53:52 +0800

    add event

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import argparse
 from utils.bcuzip import bcuzip
-
+from utils.event import event
 
 def main():
     parser = argparse.ArgumentParser()
@@ -9,11 +9,17 @@ def main():
     parser_bcuzip = subparsers.add_parser("bcuzip", help="Process a file or folder of certain game emulator files")
     parser_bcuzip.add_argument("--file", help="Path to a certain game emulator file")
     parser_bcuzip.add_argument("--folder", help="Path to a certain game emulator folder")
+    
+    parser_event = subparsers.add_parser("event", help="Get events of .tsv files of a certain game")
+    parser_event.add_argument("--old", action="store_true", help="Get files from the old way")
+    parser_event.add_argument("--new", action="store_true", help="Get files from the new way")
 
     args = parser.parse_args()
 
     if args.command == "bcuzip":
         bcuzip(file=args.file, folder=args.folder)
+    elif args.command == "event":
+        event(old=args.old, new=args.new)
     else:
         parser.print_help()
 
---
+++ utils/event.py
@@ -0,0 +1,205 @@
+import os, json
+import random, time, datetime
+import requests, hashlib, hmac
+
+class EventOld:
+    def __init__(self, cc: str, file: str):
+        cc = "" if cc == "jp" else cc
+        self.aws_access_key_id = "AKIAJCUP3WWCHRJDTPPQ"
+        self.aws_secret_access_key = "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"
+        self.region = "ap-northeast-1"
+        self.service = "s3"
+        self.request = "aws4_request"
+        self.algorithm = "AWS4-HMAC-SHA256"
+        self.domain = "nyanko-events-prd.s3.ap-northeast-1.amazonaws.com"
+        self.url = f"https://{self.domain}/battlecats{cc}_production/{file}.tsv"
+
+    def get_auth_header(self):
+        output = self.algorithm + " "
+        output += f"Credential={self.aws_access_key_id}/{self.get_date()}/{self.region}/{self.service}/{self.request}, "
+        output += f"SignedHeaders=host;x-amz-content-sha256;x-amz-date, "
+        signature = self.get_signing_key(self.get_amz_date())
+        output += f"Signature={signature.hex()}"
+        return output
+
+    def get_date(self):
+        return datetime.datetime.utcnow().strftime("%Y%m%d")
+
+    def get_amz_date(self):
+        return datetime.datetime.utcnow().strftime("%Y%m%dT%H%M%SZ")
+
+    def get_signing_key(self, amz: str):
+        k = ("AWS4" + self.aws_secret_access_key).encode()
+        k_date = self.hmacsha256(k, self.get_date())
+        date_region_key = self.hmacsha256(k_date, self.region)
+        date_region_service_key = self.hmacsha256(date_region_key, self.service)
+        signing_key = self.hmacsha256(date_region_service_key, self.request)
+
+        string_to_sign = self.get_string_to_sign(amz)
+
+        final = self.hmacsha256(signing_key, string_to_sign)
+        return final
+
+    def hmacsha256(self, key: bytes, message: str) -> bytes:
+        return hmac.new(key, message.encode(), hashlib.sha256).digest()
+
+    def get_string_to_sign(self, amz: str):
+        output = self.algorithm + "\n"
+        output += amz + "\n"
+        output += (
+            self.get_date()
+            + "/"
+            + self.region
+            + "/"
+            + self.service
+            + "/"
+            + self.request
+            + "\n"
+        )
+        request = self.get_canonical_request(amz)
+        output += hashlib.sha256(request.encode()).hexdigest()
+        return output
+
+    def get_canonical_request(self, amz: str):
+        output = "GET\n"
+        output += self.get_canonical_uri() + "\n" + "\n"
+        output += "host:" + self.domain + "\n"
+        output += "x-amz-content-sha256:UNSIGNED-PAYLOAD\n"
+        output += "x-amz-date:" + amz + "\n"
+        output += "\n"
+        output += "host;x-amz-content-sha256;x-amz-date\n"
+        output += "UNSIGNED-PAYLOAD"
+        return output
+
+    def get_canonical_uri(self):
+        return self.url.split(self.domain)[1]
+
+    def make_request(self):
+        url = self.url
+        headers = {
+            "accept-encoding": "gzip",
+            "authorization": self.get_auth_header(),
+            "connection": "keep-alive",
+            "host": self.domain,
+            "user-agent": "Dalvik/2.1.0 (Linux; U; Android 9; Pixel 2 Build/PQ3A.190801.002)",
+            "x-amz-content-sha256": "UNSIGNED-PAYLOAD",
+            "x-amz-date": self.get_amz_date(),
+        }
+
+        return requests.get(url, headers=headers)
+
+    def to_file(self):
+        response = self.make_request()
+        return response.content
+
+class EventNew:
+    def __init__(self, file: str, cc: str):
+        cc = "" if cc == "jp" else cc
+        self.accountCode = None
+        self.password = None
+        self.passwordRefreshToken = None
+        self.jwtToken = None
+        self.tokenCreatedAt = None
+        self.newEventLink = f"https://nyanko-events.ponosgames.com/battlecats{cc}_production/{file}.tsv?jwt="
+        self.userCreateLink = "https://nyanko-backups.ponosgames.com/?action=createAccount&referenceId="
+        self.passwordLink = "https://nyanko-auth.ponosgames.com/v1/users"
+        # self.passwordRefreshLink = "https://nyanko-auth.ponosgames.com/v1/user/password"
+        self.jwtLink = "https://nyanko-auth.ponosgames.com/v1/tokens"
+
+    def generateRandomHex(self, length: int) -> str:
+        return ''.join(random.choice('0123456789abcdef') for _ in range(length))
+
+    def hmacSha256(self, key, content):
+        return hmac.new(key, str(content).encode('utf-8'), hashlib.sha256).digest()
+
+    def getNyankoSignature(self, json_text):
+        random_data = self.generateRandomHex(64)
+        return random_data + self.hmacSha256(bytes(self.accountCode + random_data, 'utf-8'), json_text).hex()
+
+    def getPostResponse(self, url, json_text):
+        headers = {
+            "Nyanko-Signature": self.getNyankoSignature(json_text),
+            "Nyanko-Signature-Version": "1",
+            "Nyanko-Signature-Algorithm": "HMACSHA256",
+            "Content-Type": "application/json",
+            "Nyanko-Timestamp": str(int(time.time()* 1000)),
+            "User-Agent": "Dalvik/2.1.0 (Linux; U; Android 13; XQ-BC52 Build/61.2.A.0.447)",
+            "Connection": "Keep-Alive",
+            "Accept-Encoding": "gzip"
+        }
+        return requests.post(url=url, headers=headers, data=json_text.encode("utf-8"))
+
+    def generateAccount(self):
+        self.accountCode = None
+        self.password = None
+        self.passwordRefreshToken = None
+        acc = requests.get(self.userCreateLink)
+        if acc.json()['success'] == True:
+            self.accountCode = acc.json()['accountId']
+        passwordHeaderData = {
+            'accountCode': self.accountCode,
+            'accountCreatedAt': str(int(time.time())),
+            'nonce': self.generateRandomHex(32),
+        }
+        passwordResponse = self.getPostResponse(self.passwordLink, json.dumps(passwordHeaderData).replace(" ", ""))
+        if passwordResponse.json()['statusCode'] == 1:
+            self.password = passwordResponse.json()['payload']['password']
+            self.passwordRefreshToken = passwordResponse.json()['payload']['passwordRefreshToken']
+
+    def generateJWTToken(self):
+        if self.accountCode == None or self.password == None:
+            return
+        tokenData = {
+            'accountCode': self.accountCode,
+            'clientInfo': {
+            'client':{
+                'countryCode': 'ja',
+                'version': '999999',
+                },
+            'device':{
+                'model': 'XQ-BC52'
+                },
+            'os':{
+                'type': 'android',
+                'version': 'Android 13'
+                }
+            },
+            'nonce': self.generateRandomHex(32),
+            'password': self.password
+        }
+        tokenResponse = self.getPostResponse(self.jwtLink, json.dumps(tokenData).replace(" ", ""))
+        if tokenResponse.json()['statusCode'] == 1:
+            return tokenResponse.json()['payload']['token']
+        
+    def to_file(self):
+        self.generateAccount()
+        self.jwtToken = self.generateJWTToken()
+        self.tokenCreatedAt = int(time.time() * 1000)
+        data = requests.get(self.newEventLink + self.jwtToken).content
+        return data
+
+
+def event(old=None, new=None):
+    cc = ["jp", "tw", "en", "kr"]
+    file_types = ["sale", "gatya", "item"]
+
+    if old:
+        for c in cc:
+            for f in file_types:
+                process(EventOld, c, f)
+
+    if new:
+        for c in cc:
+            for f in file_types:
+                process(EventNew, c, f)
+                    
+def process(cls, cc: str, file: str):
+    e = cls(file=file, cc=cc)
+    data = e.to_file()
+
+    base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+    target_dir = os.path.join(base_dir, "Data", "event")
+    file = os.path.join(target_dir, f"{cc.upper()}_{file}.tsv")
+    
+    with open(file, "wb") as f:
+        f.write(data)
