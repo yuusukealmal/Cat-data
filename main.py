ca0df65c307fc5a1a7e31a24f1337e58ec626dff
侑介 <spark9487111@gmail.com>
2024-11-28 22:53:47 +0800

    conflict

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ def main():
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
 
+    parser_bcuzip = subparsers.add_parser("bcuzip", help="Process a file or folder of certain game emulator files")
+    parser_bcuzip.add_argument("--file", help="Path to a certain game emulator file")
+    parser_bcuzip.add_argument("--folder", help="Path to a certain game emulator folder")
 
     args = parser.parse_args()
 
