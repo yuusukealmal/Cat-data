b1a4c975737e9423a8a248f0f964cdd85da85aef
侑介 <spark9487111@gmail.com>
2024-12-04 01:29:27 +0800

    remove unuse lib

--- utils/cloudfront.py
+++ utils/cloudfront.py
@@ -1,5 +1,4 @@
 import time, json, base64
-import requests
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes, serialization
 from cryptography.hazmat.primitives.asymmetric import padding
