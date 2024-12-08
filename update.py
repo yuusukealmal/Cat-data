b9df9d2f4f5fba59098b93d7640f8b1583555245
侑介 <spark9487111@gmail.com>
2024-12-08 19:59:24 +0800

    add git push

--- update.py
+++ update.py
@@ -1,6 +1,22 @@
 from utils.event import event
 from utils.local import local
 
+import os
+from dotenv import load_dotenv
+from git import Repo
+
+def git_push():
+    load_dotenv()
+    try:
+        repo = Repo(os.getenv("REPO"))
+        repo.git.add(update=True)
+        repo.index.commit("Update Certain Game Data")
+        origin = repo.remote(name='main')
+        origin.push()
+    except Exception as e:
+        print('Some error occured while pushing the code', e)    
+
 if __name__ == "__main__":
     event(new=True)
     local(way="latest", remote=True)
+    git_push()
