8e8f925a129f3f5675f1cfa3167770e79b83cdab
侑介 <spark9487111@gmail.com>
2024-12-08 20:28:02 +0800

    test commit

--- update.py
+++ update.py
@@ -9,12 +9,18 @@ def git_push():
     load_dotenv()
     try:
         repo = Repo(os.getenv("REPO"))
-        repo.git.add(update=True)
-        repo.index.commit("Update Certain Game Data")
-        origin = repo.remote(name='main')
-        origin.push()
+
+        if repo.is_dirty(untracked_files=True) or repo.index.diff(None):
+            repo.git.add(update=True)
+            repo.index.commit("test commit")
+            origin = repo.remote(name='origin')
+            origin.push()
+            print("Changes were pushed to the repository.")
+        else:
+            print("No changes to commit or push.")
+
     except Exception as e:
-        print('Some error occured while pushing the code', e)    
+        print('Some error occurred while pushing the code:', e) 
 
 if __name__ == "__main__":
     event(new=True)
