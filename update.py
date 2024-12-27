16dfa31b49990578c94a2fdbb7106acf93f6b8ef
侑介 <spark9487111@gmail.com>
2024-12-27 21:47:41 +0800

    fix

--- update.py
+++ update.py
@@ -8,7 +8,7 @@ from git import Repo
 def git_push():
     load_dotenv()
     try:
-        repo = Repo("/root/Cat-data/.git")
+        repo = Repo(os.getenv("REPO"))
 
         if repo.is_dirty(untracked_files=True) or repo.index.diff(None):
             repo.git.add(update=True)
